2242ef733a6dcd680baf021990dae357169d4fb1
YueweiMa <108724061+Yue-wei-Ma@users.noreply.github.com>
2023-02-21 11:30:57 +0800

    Update 013_BubbleSort.py

--- 013_BubbleSort.py
+++ 013_BubbleSort.py
@@ -4,7 +4,15 @@ arr=[64,34,25,12,22,11,90]
 import random
 li=[i+1 for i in range(10)]
 """
-li=[90,64,34,25,12,22,11]
-
+import random
+def bubble_sort(li):
+    for i in range(len(li)-1):
+        for j in range(len(li)-i-1):
+            if li[j]>li[j+1]: #降序时改成<
+                li[j],li[j+1]=li[j+1],li[j]
+                print(li)
+li=[random.randint(0,10000) for i in range(1000)]
+print(li)
+bubble_sort(li)
 print(li)
 
